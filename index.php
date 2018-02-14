6382eebd1d16391d629491ba0b3ca186a993a720
Harrison Hess <the42ndturtle@gmail.com>
2018-02-14 13:39:44 -0500

    some changes

--- index.php
+++ index.php
@@ -1,13 +1,23 @@
 <html>
   <head>
-    <link rel="stylesheet" type="text/css" href="style.css" />
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="style.css" />
     <title>Florida Man Game</title>
   </head>
   <body>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="script.js"></script>
-      <div>
-        test
+    <div id="laoding"></div>
+    <div id="account-button-wrapper">
+      <div id="sign-up" class="account-button">
+        <h3 align="center" class="account-button-text">Sign Up</h1>
       </div>
+      <div id="log-in" class="account-button">
+        <h3 align="center" class="account-button-text">Log In</h1>
+      </div>
+    </div>
+    <div id="play">
+      <h1 align="center" id="play-text">Play</h1>
+    </div>
   </body>
 </html>
