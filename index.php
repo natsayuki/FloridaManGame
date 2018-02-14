6e3942d521cac1c80153db86d6846c275267cfbd
Harrison Hess <the42ndturtle@gmail.com>
2018-02-14 11:42:48 -0500

    better

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <title>Florida Man Game</title>
+  </head>
+  <body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="script.js"></script>
+      <div>
+        test
+      </div>
+  </body>
+</html>
